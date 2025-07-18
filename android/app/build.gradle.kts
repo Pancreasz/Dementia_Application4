5c3de866b9cb2706ab523d12bd0ed7b2b8a0dcec
jayc <pummasinpp@gmail.com>
2025-07-18 18:55:13 +0700

    network update

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -9,7 +9,7 @@ android {
     namespace = "com.example.new_moca"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
-
+    ndkVersion = "27.0.12077973"
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
